7fa2af72542ae2f213c3d98bfb795ec0d26da265
Satnam Meena <satnam.meena2004@gmail.com>
2026-04-05 17:26:05 +0530

    Negative positive testing

--- tests/LoginTests.cs
+++ tests/LoginTests.cs
@@ -1,39 +1,69 @@
 
+
+
+
+
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
-using NUnit.Framework;
+using Microsoft.VisualBasic;
 
-namespace EssentialTests;
-
-public class EssentialTests : PageTest
+public class LoginTest : PageTest
 {
-    [Test]
-    public async Task HomePageTitleIsCorrect()
+    private readonly string BASE_URL = "https://practicesoftwaretesting.com";
+
+    [SetUp]
+    public async Task SetupNavigateToWebSite()
     {
-        await Page.GotoAsync("https://demo.playwright.dev/todomvc");
-        await Expect(Page).ToHaveTitleAsync("React • TodoMVC");
+        await Page.GotoAsync(BASE_URL + "/auth/login");
     }
 
     [Test]
-    public async Task NaviagationMenuIsVisible()
+
+    public async Task ValidLoginShouldRedirectToDashboard()
     {
-        await Page.GotoAsync("https://practicesoftwaretesting.com");
-        var nav = Page.Locator("nav");
-        await Expect(nav).ToBeVisibleAsync();
+        var email = Page.Locator("//input[@id='email']");
+        var pass = Page.Locator("//input[@id='password']");
+        var btn = Page.Locator("//input[@value='Login']");
+        await email.FillAsync("xirovo4283@fengnu.com");
+        await pass.FillAsync("Xirovo4283@fengnu.com");
+        await btn.ClickAsync();
+        await Expect(Page).ToHaveURLAsync(new System.Text.RegularExpressions.Regex(".*/account"));
+
+    }
+    [Test]
+    public async Task InvalidLoginShould_ShowTheHelpMessage()
+    {
+        var email = Page.Locator("//input[@id='email']");
+        var pass = Page.Locator("//input[@id='password']");
+        var btn = Page.Locator("//input[@value='Login']");
+        var help_message = Page.Locator("//div[@class='help-block']");
+        await email.FillAsync("xir@fengnu.com");
+        await pass.FillAsync("Xiroo.com");
+        await btn.ClickAsync();
+        await Expect(help_message).ToBeVisibleAsync();
 
     }
 
     [Test]
-    public async Task ScreenShotOnLoad()
+    public async Task EmptyFieldLogin_BlockLogin()
     {
-        await Page.GotoAsync("https://practicesoftwaretesting.com");
-        var parent = Directory.GetParent(Directory.GetCurrentDirectory());
-        var path = Path.Combine(
-    AppContext.BaseDirectory, "..", "..", "..", "Artifacts", "home.png"
-);
-        await Page.ScreenshotAsync(new() { Path = path });
+        var btn = Page.Locator("//input[@value='Login']");
+        await btn.ClickAsync();
+        var email_error = Page.Locator("//div[@id='email-error']");
+        await Expect(email_error).ToBeVisibleAsync();
 
-        Assert.Pass("Screetshot captrued successfully");
     }
+    [Test]
+    public async Task ValidLogin_ThenLogout()
+    {
+        await ValidLoginShouldRedirectToDashboard();
+        var menu = Page.Locator("//a[@id='menu']");
+        await menu.ClickAsync();
+        var log_out = Page.GetByText("Sign Out");
+        await log_out.ClickAsync();
+        await Expect(Page).ToHaveURLAsync(new System.Text.RegularExpressions.Regex(".*/login"));
+
+    }
+
+
 }
-
