ef802247bebfca8810d0c6a9002e71c1d83e0029
Satnam Meena <satnam.meena2004@gmail.com>
2026-04-05 16:37:31 +0530

    Essential Tests

--- tests/LoginTests.cs
+++ tests/LoginTests.cs
@@ -3,14 +3,37 @@ using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
 
+namespace EssentialTests;
 
-public class LoginTest : PageTest
+public class EssentialTests : PageTest
 {
     [Test]
-    public async Task homePageTitleIsCorrect()
+    public async Task HomePageTitleIsCorrect()
     {
         await Page.GotoAsync("https://demo.playwright.dev/todomvc");
         await Expect(Page).ToHaveTitleAsync("React • TodoMVC");
+    }
+
+    [Test]
+    public async Task NaviagationMenuIsVisible()
+    {
+        await Page.GotoAsync("https://practicesoftwaretesting.com");
+        var nav = Page.Locator("nav");
+        await Expect(nav).ToBeVisibleAsync();
+
+    }
+
+    [Test]
+    public async Task ScreenShotOnLoad()
+    {
+        await Page.GotoAsync("https://practicesoftwaretesting.com");
+        var parent = Directory.GetParent(Directory.GetCurrentDirectory());
+        var path = Path.Combine(
+    AppContext.BaseDirectory, "..", "..", "..", "Artifacts", "home.png"
+);
+        await Page.ScreenshotAsync(new() { Path = path });
+
+        Assert.Pass("Screetshot captrued successfully");
     }
 }
 
